7c7bc3d2a9a1b13bf2a967c1d255dddc16bfc1c3
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-04-14 14:44:53 +0530

    no change

--- Codeforces/1.cpp
+++ Codeforces/1.cpp
@@ -6,34 +6,34 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int n;
-    cin>>n;
-    vector<int> colors(n+2,1);
+    cin >> n;
+    vector<int> colors(n + 2, 1);
     int i;
-    for(i = 2; i*i <= n+1; i++)
+    for (i = 2; i * i <= n + 1; i++)
     {
-        if(colors[i] == 1)
+        if (colors[i] == 1)
         {
-            for(int j = i*i; j <=n+1; j+=i)
+            for (int j = i * i; j <= n + 1; j += i)
             {
-                if(colors[j] == 1)
-                    colors[j] = colors[i]+1;
+                if (colors[j] == 1)
+                    colors[j] = colors[i] + 1;
             }
         }
     }
 
     vector<int> c = colors;
-    sort(c.begin(),c.end());
+    sort(c.begin(), c.end());
     int no = 0;
-    for(int i = 0; i < n+2; i++)
+    for (int i = 0; i < n + 2; i++)
     {
-        if(c[i] != c[i+1])
+        if (c[i] != c[i + 1])
             no++;
     }
-    cout<<no<<"\n";
-    for(i = 2; i <= n+1; i++)
+    cout << no << "\n";
+    for (i = 2; i <= n + 1; i++)
     {
-        cout<<colors[i]<<" ";
+        cout << colors[i] << " ";
     }
-    cout<<"\n";
+    cout << "\n";
     return 0;
 }
