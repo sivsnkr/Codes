b1d9112b1e10aad29bc0a25bf8c5fdf5b8860aee
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-04-16 16:01:16 +0530

    update

--- atcoder/1.cpp
+++ atcoder/1.cpp
@@ -1,26 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int maxv(vector<pair<int,int>> &items, int w, int n)
+int maxv(vector<pair<int, int>> &items, int w, int n, map<pair<int, int>, int> &me)
 {
-    if(w == 0||n == 0)
+    if (w == 0 || n == 0)
         return 0;
-    else if(items[n-1].first <= w)
-        return max(items[n-1].second+maxv(items,w-items[n-1].first,n-1),maxv(items,w,n-1));
-    return maxv(items,w,n-1);
+    if (me.find({n, w}) != me.end())
+        return me[{n, w}];
+    if (items[n - 1].first <= w)
+        return me[{n, w}] = max(items[n - 1].second + maxv(items, w - items[n - 1].first, n - 1, me), maxv(items, w, n - 1, me));
+    return me[{n, w}] = maxv(items, w, n - 1, me);
 }
 
 int main()
 {
-    int n,w;cin>>n>>w;
-    vector<pair<int,int>> items(n);
-    for(auto &a : items)
+    int n, w;
+    cin >> n >> w;
+    vector<pair<int, int>> items(n);
+    for (auto &a : items)
     {
-        cin>>a.first>>a.second;
+        cin >> a.first >> a.second;
     }
 
-    int m = maxv(items,w,n);
-    cout<<m<<"\n";
+    map<pair<int, int>, int> me;
+    maxv(items, w, n, me);
+    cout << me[{n, w}] << "\n";
 
     // int weight = 0;
     // long long int k[n+1][w+1];
