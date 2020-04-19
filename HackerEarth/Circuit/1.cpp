dd57c60a8f7842e1b5e65f47d1df22be22dae82d
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-04-19 11:04:17 +0530

    update

--- HackerEarth/Circuit/1.cpp
+++ HackerEarth/Circuit/1.cpp
@@ -1,115 +1,66 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-void dfs(int src, vector<unordered_set<int>> &graph, vector<bool> &visited, set<int> &cycle)
+string b(int n)
 {
-    visited[src] = 1;
-    unordered_set<int>::iterator it;
-
-    for(it = graph[src].begin(); it != graph[src].end(); it++)
+    string res = "";
+    while(n > 1)
     {
-        if(visited[*it])
-            cycle.insert(*it);
-        else
-        {
-            dfs(*it,graph,visited,cycle);
-        }
+        res+=(char(n%2) + '0');
+        n/=2;
     }
+    res+=(char(n) + '0');
+    int si = res.length();
+    for(int i = si; i < 16; i++)
+    {
+        res+='0';
+    }
+    // cout<<res.length()<<"\n";
+    reverse(res.begin(),res.end());
+    // cout<<res<<"\n";
+    return res;
 }
 
-void getnodes(int src, vector<unordered_set<int>> &graph, vector<bool> &visited, int &noofnodes)
+void rot(string &s, int m)
 {
-    visited[src] = true;
-    unordered_set<int>::iterator it;
+    reverse(s.begin(),s.begin()+m);
+    reverse(s.begin()+m,s.end());
+    reverse(s.begin(),s.end());
+}
 
-    for(it = graph[src].begin(); it != graph[src].end(); it++)
+int btoi(string s){
+    int sum = 0;
+    reverse(s.begin(),s.end());
+    for(int i = 0; i < 16; i++)
     {
-        if(!visited[src])
-        {
-            noofnodes++;
-            getnodes(*it,graph,visited,noofnodes);
-        }
+        sum+=(int(s[i])-'0')*pow(2,i);
     }
-}
-
-int power(int x, unsigned int y, unsigned int m) 
-{ 
-    if (y == 0) 
-        return 1; 
-    int p = power(x, y/2, m) % m; 
-    p = (p * p) % m; 
-  
-    return (y%2 == 0)? p : (x * p) % m; 
-}
-
-int modInverse(int a, int m) 
-{ 
-    return power(a, m-2, m);
+    return sum;
 }
 
 int main()
 {
-    int nodes,edges;
-    cin>>nodes>>edges;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
 
-    nodes++;
-    vector<unordered_set<int>> graph(nodes);
-
-    int i;
-    for(i  = 0; i < edges; i++)
+    // all the code goes here
+    int t;cin>>t;
+    while(t--)
     {
-        int src, dest; 
-        cin>>src>>dest;
-        graph[src].insert(dest);
-    }
+        int n,m;cin>>n>>m;
+        char c; cin>>c;
 
-    vector<bool> visited(nodes,0);
-    set<int> cycle;
-    vector<int> cyclenodes;
-    dfs(1,graph,visited,cycle);
-
-    for(auto i : cycle)
-    {
-        cyclenodes.push_back(i);
-    }
-    int noofedges = cycle.size()-1;
-
-    if(edges == 0)
-        cout<<0<<" "<<0<<endl;
-    else
-    {
-        int a = 0,b = 0;
-        for(i = 0; i <= noofedges; i++)
+        string s = b(n);
+        m%=16;
+        if(c == 'L')
+            rot(s,m);
+        else
         {
-            for(int j = i+1; j <= noofedges; j++)
-            {
-                // get nodes in parnet
-                graph[cyclenodes[i]].erase(cyclenodes[j]);
-                int parentnodes,childnodes;
-                vector<bool> visited(noofedges,false);
-                getnodes(i,graph,visited,parentnodes);
-                visited.assign(noofedges,false);
-                getnodes(j,graph,visited,childnodes);
-                if(parentnodes == childnodes)
-                {
-                    if(parentnodes % 2 == 0)
-                    {
-                        a++;
-                    }
-                }
-                else
-                {
-                    b++;
-                }
-                graph[cyclenodes[i]].insert(cyclenodes[j]);
-            }
+            rot(s,16-m);
         }
-
-        int c = 1000000007;
-        int totalinverse = modInverse(noofedges,c);
-        totalinverse = (totalinverse%c);
-        a = ((a%c)*totalinverse)%c;
-        b = ((b%c)*totalinverse)%c;
-        cout<<a<<" "<<b<<endl;
+        int in = btoi(s);
+        cout<<in<<"\n";
     }
+
+    return 0;
 }
