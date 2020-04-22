a10e8915af3c866caafd4e8c3ab5ffc4caf516de
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-04-22 11:59:25 +0530

    update

--- atcoder/2.cpp
+++ atcoder/2.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef long l;
 
 int main()
 {
@@ -8,8 +9,28 @@ int main()
     cin.tie(0);cout.tie(0);
 
     // all the code goes here
+    int n;cin>>n;
 
-    ll i = 5676578678978645;
-    cout<<i<<"\n";
+    vector<int> a(n);
+    for(int &i : a)
+    {
+        cin>>i;
+    }
+
+    int dp[n];
+    memset(dp,0,sizeof(dp));
+
+    ll sum = 0;
+    dp[0] = 0;dp[1] = a[0]+a[1];
+
+    for(int i = 2; i < n; i++)
+    {
+        dp[i] = min(a[i]+a[i-1],dp[i-1]+a[i]);
+    }
+
+    for(int a : dp)
+        cout<<a<<" ";
+    cout<<"\n";
+
     return 0;
 }
