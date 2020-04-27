190ea22997cca135b093736858d94e09c65be70b
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-04-27 21:15:36 +0530

    modify cp_template.cpp file

--- cp_template.cpp
+++ cp_template.cpp
@@ -1,13 +1,15 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 typedef long l;
 const char nl = '\n';
+typedef unsigned u;
 
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(0);cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
 
     // all the code goes here
 
