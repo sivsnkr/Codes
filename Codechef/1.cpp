bebdd7c9c3cf0a7ea2ce4bf99d3799d700d8fe08
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-05-02 10:58:23 +0530

    add bitmasking file containg simple but frequently used tools for bitmasking

--- Codechef/1.cpp
+++ Codechef/1.cpp
@@ -12,6 +12,20 @@ int main()
 
     // all the code goes here
     int t;
-
+    scanf("%d",&t);
+    while(t > 0)
+    {
+        ll x,y,l,r;
+        scanf("%lld%lld%lld%lld",&x,&y,&l,&r);
+        ll num = (ll(1)<<62)-ll(1);
+        for(int i = 62; i >= 0; i--)
+        {
+            if(num>=l&&num<=r)
+                break;
+            num &= ~(ll(1)<<i);
+        }
+        printf("%lld\n",num);
+        t--;
+    }
     return 0;
 }
