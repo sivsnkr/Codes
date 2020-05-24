04025f678d6ea4be09e65ce3041819a7cbd35455
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-05-24 09:12:15 +0530

    update

--- Codeforces/1.cpp
+++ Codeforces/1.cpp
@@ -3,48 +3,55 @@ using namespace std;
 typedef long long LL;
 typedef long L;
 const char NL = '\n';
-#define f(i,a,b) for(int i=a;i<b;i++)
-#define fr(i,a,b) for(int i=a;i>=b;i--)
-#define testf int t; scanf("%d",&t); while(t--)
-#define test int t; cin>>t; while(t--)
+#define f(i, a, b) for (int i = a; i < b; i++)
+#define fr(i, a, b) for (int i = a; i >= b; i--)
+#define testf        \
+	int t;           \
+	scanf("%d", &t); \
+	while (t--)
+#define test  \
+	int t;    \
+	cin >> t; \
+	while (t--)
 #define mod 1000000007
-#define all(a) a.begin(),a.end()
+#define all(a) a.begin(), a.end()
 #define size(container) (int)container.size()
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    // all the code goes here
-	int n,a,r,m;
-	cin>>n>>a>>r>>m;
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	// all the code goes here
+	int n, a, r, m;
+	cin >> n >> a >> r >> m;
 	int sum = 0;
 	vector<int> arr(n);
-	for(int &it : arr)cin>>it,sum+=it;
+	for (int &it : arr)
+		cin >> it, sum += it;
 	int mh = *min_element(all(arr));
 	int mxh = *max_element(all(arr));
 	int mincost = INT_MAX;
-	f(height,mh,mxh+1)
+	f(height, mh, mxh + 1)
 	{
 		int cost = 0;
-		int optotal = height*n;
-		if(optotal > sum)
+		int optotal = height * n;
+		if (optotal > sum)
 		{
-			cost+=(optotal-sum)*a;
-			cost-=(optotal-sum)*m;
+			cost += (optotal - sum) * a;
+			cost -= (optotal - sum) * m;
 		}
-		else if(sum > optotal)
+		else if (sum > optotal)
 		{
-			cost+=(sum-optotal)*r;
-			cost-=(sum-optotal)*m;
+			cost += (sum - optotal) * r;
+			cost -= (sum - optotal) * m;
 		}
-		f(i,0,n)
+		f(i, 0, n)
 		{
-			cost+=abs(arr[i]-height)*m;
+			cost += abs(arr[i] - height);
 		}
-		mincost = min(mincost,cost);
+		mincost = min(mincost, cost);
 	}
-	cout<<mincost<<endl;
-    return 0;
+	cout << mincost << endl;
+	return 0;
 }
