53caa556fbf52e0e4021e255b43f21981b006580
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-05-25 16:47:11 +0530

    update

--- Codeforces/1.cpp
+++ Codeforces/1.cpp
@@ -10,57 +10,64 @@ const char NL = '\n';
 #define mod 1000000007
 #define all(a) a.begin(),a.end()
 #define size(container) (int)container.size()
-#define int long long int
 
-vector<int> sums;
-vector<int> heights;
-int n,a,r,m;
-
-int solve(int height)
-{
-	int h = upper_bound(heights.begin(), heights.end(), height) - heights.begin();
-	int p = height*h - sums[h-1];
-	int q = sums[n-1]-sums[h-1]-height*(n-h);
-	int cost = LLONG_MAX;
-	if(p >= q)
-	{
-		int tcost = (p-q)*a+q*m;
-		cost = min(tcost,cost);
-	}
-	if(q >= p)
-	{
-		int tcost = (q-p)*r+p*m;
-		cost = min(cost,tcost);
-	}
-	return cost;
-}
-
-int32_t main()
+int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
     // all the code goes here
-	cin>>n>>a>>r>>m;
-	m = min(m,a+r);
-	heights.assign(n,0);
-	f(i,0,n)cin>>heights[i];
-	sort(all(heights));
-	sums.assign(n,0);
-	sums[0] = heights[0];
-	f(i,1,n)
-	{
-		sums[i] = sums[i-1]+heights[i];
+	test{
+		int n;
+		cin>>n;
+		vector<int> m(n);
+		for(int &it : m)cin>>it;
+		sort(all(m));
+		vector<int> dp(n,0);
+		for(int i = 0; i < n; i+=2)
+		{
+			if(m[i+1]-m[i] == 1)
+			{
+				dp[i] = -1;
+				dp[i+1] = -1;
+			}
+		}
+
+		f(i,0,n)
+		{
+			if(dp[i] == 0)
+			{
+				dp[i] = m[i]%2;
+			}
+		}
+
+		int onem = 0;
+		int one = 0;
+		int zero = 0;
+		f(i,0,n)
+		{
+			switch (dp[i])
+			{
+			case -1:
+				onem++;
+				break;
+			case 0:
+				zero++;
+				break;
+			case 1:
+				one++;
+				break;
+			}
+		}
+
+		if(one%2 == 0 && onem%2 == 0 && zero%2 == 0 && one+zero+onem == n)
+			cout<<"YES";
+		else
+		{
+			cout<<"NO";
+		}
+		
+		cout<<endl;
 	}
-
-	int ans = solve(sums[n-1]/n);
-	ans = min(ans,solve((sums[n-1])/n+1));
-
-	f(i,0,n)
-	{
-		ans = min(ans,solve(heights[i]));
-	}
-
-	cout<<ans<<endl;
     return 0;
 }
--- UVa/1.cpp
+++ UVa/1.cpp
@@ -1,19 +1,84 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef long l;
-const char nl = '\n';
+typedef long long LL;
+typedef long L;
+const char NL = '\n';
+#define f(i,a,b) for(int i=a;i<b;i++)
+#define fr(i,a,b) for(int i=a;i>=b;i--)
+#define testf int t; scanf("%d",&t); while(t--)
+#define test int t; cin>>t; while(t--)
+#define mod 1000000007
+#define all(a) a.begin(),a.end()
+#define size(container) (int)container.size()
+#define int long long int
 
-int main()
+int32_t main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-
     // all the code goes here
-    priority_queue<int,vector<int>,greater<int>> q;
-    q.push(10);
-    q.push(9);
-    printf("%d\n",q.top());
+    int row,column;
+    while(!cin.eof() && cin>>row>>column)
+    {
+        vector<vector<pair<int,int>>> mat(row);
+        f(i,0,row)
+        {
+            int n;
+            cin>>n;
+            f(j,0,n)
+            {
+                int ro;
+                cin>>ro;
+                mat[i].push_back({ro-1,0});
+            }
+            f(j,0,n)
+            {
+                int value;
+                cin>>value;
+                mat[i][j].second = value;
+            }
+        }
+
+        vector<vector<pair<int,int>>> transpose(column);
+        f(i,0,row)
+        {
+            int n = size(mat[i]);
+            f(j,0,n)
+            {
+                transpose[mat[i][j].first].push_back({i+1,mat[i][j].second});
+            }
+        }
+
+        cout<<column<<" "<<row<<NL;
+        f(i,0,column)
+        {
+            int n = size(transpose[i]);
+            if(n > 0)
+                cout<<n<<" ";
+            else
+            {
+                cout<<n;
+            }
+            
+            bool in = false;
+            f(j,0,n-1)
+            {
+                cout<<transpose[i][j].first<<" ";
+            }
+            if(n > 0)
+                cout<<transpose[i][n-1].first;
+            cout<<NL;
+            in = false;
+            f(j,0,n-1)
+            {
+                cout<<transpose[i][j].second<<" ";
+            }
+            if(n > 0)
+                cout<<transpose[i][n-1].second;
+            cout<<NL;
+        }
+        fflush(stdin);
+    }
     return 0;
 }
