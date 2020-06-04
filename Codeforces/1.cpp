46636284f5e3619c776e6cfda30b05da42a8c2b5
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-06-04 08:59:43 +0530

    update cp_template

--- Codeforces/1.cpp
+++ Codeforces/1.cpp
@@ -10,53 +10,7 @@ const char NL = '\n';
 #define mod 1000000007
 #define all(a) a.begin(),a.end()
 #define size(container) (int)container.size()
-#define int long long
-
-string decimalToBinary(int N,int m) 
-{
-	// cout<<"N "<<N<<endl;
-	string res = "";
-	while(N != 0)
-	{
-		res+=(char(N%2)+48);
-		// cout<<"res "<<res<<endl;
-		N = N>>1;
-	}
-	while(res.length() < m)
-	{
-		res = res+'0';
-	}
-	reverse(all(res));
-	return res;
-}
-
-void solve()
-{
-	int n,m;
-	cin>>n>>m;
-	vector<int> st(n);
-	f(i,0,n)
-	{
-		string s;
-		cin>>s;
-		st[i] = stoll(s,0,2);
-	}
-	sort(all(st));
-	int ls = pow(2,m)-1;
-	int length = ls+1;
-	int mid = (length-n-1)/2;
-	f(i,0,n)
-	{
-		if(st[i] <= mid)
-			mid++;
-		else
-		{
-			break;
-		}
-		
-	}
-	cout<<decimalToBinary(mid,m)<<endl;
-}
+#define int long long int
 
 int32_t main()
 {
@@ -64,8 +18,24 @@ int32_t main()
     cin.tie(0);
     cout.tie(0);
     // all the code goes here
-	test{
-		solve();
-	}
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int &it : a)cin>>it;
+    int mx = 0;
+    int mxe = 0;
+    int sum = 0;
+    f(i,0,n)
+    {
+        sum+=a[i];
+        mxe = max(mxe,a[i]);
+        if(sum < a[i])
+        {
+            mxe = a[i];
+            sum = a[i];
+        }
+        mx = max(mx,sum-mxe);
+    }
+    cout<<mx<<endl;
     return 0;
 }
--- Codeforces/2.cpp
+++ Codeforces/2.cpp
@@ -5,34 +5,36 @@ typedef long L;
 const char NL = '\n';
 #define f(i,a,b) for(int i=a;i<b;i++)
 #define fr(i,a,b) for(int i=a;i>=b;i--)
+#define testf int t; scanf("%d",&t); while(t--)
 #define test int t; cin>>t; while(t--)
 #define mod 1000000007
 #define all(a) a.begin(),a.end()
+#define size(container) (int)container.size()
+#define int long long int
 
-int main()
+int h,c,ti;
+int32_t main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
     // all the code goes here
-    int n;
-    scanf("%d",&n);
-    vector<int> pies(n);
-    for(int &it : pies)scanf("%d",&it);
-    vector<int>total(n+1);
-    vector<int>score(n+1);
-    score[n] = pies[n-1];
-    int sum = 0;
-    for(int i = n; i > 0; i--)
-    {
-        sum+=pies[i-1];
-        total[i] = sum;
+    test{
+        cin>>h>>c>>ti;
+        if(ti <= (h+c)/2)
+        {
+            cout<<2<<NL;
+        }
+        else
+        {
+            int k = (h-ti)/(2*ti-h-c);
+            float v = abs(((float)(h*k+c*k+h)/(2*k+1))-ti);
+            k++;
+            float av = abs(((float)(h*k+c*k+h)/(2*k+1))-ti);
+            (av >= v)?cout<<2*k-1<<NL:cout<<2*k+1<<NL;
+        }
     }
-
-    for(int i = n-1; i > 0; i--)
-    {
-        score[i] = max(pies[i-1]+total[i+1]-score[i+1],score[i+1]);
-    }
-    printf("%d %d\n",total[1]-score[1],score[1]);
+    fflush(stdin);
+    fflush(stdout);
     return 0;
 }
