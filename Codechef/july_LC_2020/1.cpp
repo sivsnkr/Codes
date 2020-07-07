5326ab3a2361f123af69397f57281cf7aaeb6d02
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-07-07 08:48:53 +0530

    update

--- Codechef/july_LC_2020/1.cpp
+++ Codechef/july_LC_2020/1.cpp
@@ -6,15 +6,31 @@ const char NL = '\n';
 #define PI 3.14159265
 #define f(i, a, b) for (int i = a; i < b; i++)
 #define fr(i, a, b) for (int i = a; i >= b; i--)
-#define testf int t;scanf("%d", &t);while (t--)
-#define test int t;cin >> t;while (t--)
+#define testf        \
+    int t;           \
+    scanf("%d", &t); \
+    while (t--)
+#define test  \
+    int t;    \
+    cin >> t; \
+    while (t--)
 #define mod 1000000007
 #define all(a) a.begin(), a.end()
 #define size(container) (int)container.size()
 #define int long long int
-
+int n;
+int a[100001];
 void solve()
 {
+    test
+    {
+        cin >> n;
+        f(i, 0, n) cin >> a[i];
+        int ans = 0;
+        f(i, 1, n)
+            ans += abs(a[i] - a[i - 1]) - 1;
+        cout << ans << NL;
+    }
 }
 
 int32_t main()
