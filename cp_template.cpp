806c7778b3514328fd1295d31b8dbff7ed81b5c9
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-07-25 23:24:29 +0530

    update cp_template

--- cp_template.cpp
+++ cp_template.cpp
@@ -19,6 +19,8 @@ void solve()
 
 int32_t main()
 {
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
