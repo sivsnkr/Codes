40fc4da610b783ad7f55c4acfb949080b0c4439a
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-08-23 06:10:10 +0530

    update

---
+++ kickstart/1.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef long L;
+const char NL = '\n';
+#define PI 3.14159265
+#define f(i, a, b) for (int i = a; i < b; i++)
+#define fr(i, a, b) for (int i = a; i >= b; i--)
+#define testf int t;scanf("%d", &t);while (t--)
+#define test int t;cin >> t;while (t--)
+#define mod 1000000007
+#define all(a) a.begin(), a.end()
+#define size(container) (int)container.size()
+#define int long long int
+#define pb push_back
+#define fh freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+clock_t startTime;
+double getCurrentTime() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+void readarray(vector<int> &a)
+{
+    for(auto &it : a)cin>>it;
+}
+
+inline void solve()
+{
+    // all the code goes here
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        fh;
+    #endif
+    startTime = clock();
+    solve();
+    fflush(stdin);
+    fflush(stdout);
+    return 0;
+}
