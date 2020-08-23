352d78fe598167bd275a453becea4cd2bb8d82bc
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-08-23 16:34:56 +0530

    update

--- kickstart/2.cpp
+++ kickstart/2.cpp
@@ -1,158 +1,44 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long LL;
-typedef long L;
-const char NL = '\n';
-#define PI 3.14159265
-#define f(i, a, b) for (int i = a; i < b; i++)
-#define fr(i, a, b) for (int i = a; i >= b; i--)
-#define testf int t;scanf("%d", &t);while (t--)
-#define test int t;cin >> t;while (t--)
-#define mod 1000000007
-#define all(a) a.begin(), a.end()
-#define size(container) (int)container.size()
-// #define int long long int
-#define pb push_back
-#define fh freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
-clock_t startTime;
-double getCurrentTime() {
-	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
-}
-void readarray(vector<int> &a)
-{
-    for(auto &it : a)cin>>it;
-}
+#include<bits/stdc++.h>
 
-inline void solve()
-{
-    // all the code goes here
-    int te = 1;
-    test
-    {
-        int n,a,b,c;
-        scanf("%d%d%d%d",&n,&a,&b,&c);
-        if(a+b-c > n)
-        {
-            printf("Case #%d: IMPOSSIBLE\n",te);
-        }
-        else
-        {
-            vector<int> res(n,-1);
-            int h = n;
-            f(i,0,a-c)
-            {
-                res[i] = h-1;
-            }
-            int i = n-1;
-            int co = b-c;
-            while(co-- > 0)
-            {
-                res[i] = h-1; 
-                i--;
-            }
-            vector<int> resc = res;
-            co = c;
-            fr(i,n-1,0)
-                if(res[i]==-1 && co-- > 0)
-                {
-                    res[i] = h;
-                }
-            f(i,0,n)
-                if(res[i] == -1)
-                    res[i] = h-2;
-            // f(i,0,n)
-            //     cout<<res[i]<<" \n"[i==n-1];
-            int A = 1;
-            int mx = res[0];
-            f(i,1,n)
-            {
-                if(res[i] >= mx)
-                    A++;
-                mx = max(mx,res[i]);
-            }
-            mx = res[n-1];
-            int B = 1;
-            fr(i,n-2,0)
-            {
-                if(res[i] >= mx)
-                    B++;
-                mx = max(mx,res[i]);
-            }
-            // cout<<A<<" "<<B<<NL;
-            if(A != a || B != b)
-            {
-                // cout<<"hdfsf"<<NL;
-                // printf("Case #%d: IMPOSSIBLE\n",te);
-                int co = c;
-                f(i,0,n)
-                    if(resc[i]==-1 && co-- > 0)
-                    {
-                        resc[i] = h;
-                    }
-                f(i,0,n)
-                    if(resc[i] == -1)
-                        resc[i] = h-2;
-                // f(i,0,n)
-                //     cout<<res[i]<<" \n"[i==n-1];
-                int A = 1;
-                int mx = resc[0];
-                f(i,1,n)
-                {
-                    if(resc[i] >= mx)
-                        A++;
-                    mx = max(mx,resc[i]);
-                }
-                mx = resc[n-1];
-                int B = 1;
-                fr(i,n-2,0)
-                {
-                    if(resc[i] >= mx)
-                        B++;
-                    mx = max(mx,resc[i]);
-                }
-                if(A != a || B != b)
-                    printf("Case #%d: IMPOSSIBLE\n",te);
-                else
-                {
-                    printf("Case #%d: ",te);
-                    f(i,0,n)
-                    {
-                        if(i == n-1)
-                            printf("%d",resc[i]);
-                        else
-                            printf("%d ",resc[i]);
-                    }
-                    printf("\n");
-                }
-            }
-            else
-            {
-                printf("Case #%d: ",te);
-                f(i,0,n)
-                {
-                    if(i == n-1)
-                        printf("%d",res[i]);
-                    else
-                        printf("%d ",res[i]);
-                }
-                printf("\n");
-            }
-        }
-        te++;
-    }
-}
+int main() {
+	using namespace std;
+	ios_base::sync_with_stdio(false), cin.tie(nullptr);
 
-int32_t main()
-{
-    // ios_base::sync_with_stdio(false);
-    // cin.tie(0);
-    // cout.tie(0);
-    // #ifndef ONLINE_JUDGE
-    //     fh;
-    // #endif
-    startTime = clock();
-    solve();
-    fflush(stdin);
-    fflush(stdout);
-    return 0;
+	int T; cin >> T;
+	for (int case_num = 1; case_num <= T; case_num ++) {
+		int N, A, B, C; cin >> N >> A >> B >> C;
+
+		assert(1 <= C && C <= N);
+		assert(C <= A && A <= N);
+		assert(C <= B && B <= N);
+
+		cout << "Case #" << case_num << ": ";
+		if ((A + B - C) > N || (A + B - C == 1 && N >= 2)) {
+			cout << "IMPOSSIBLE" << '\n';
+		} else if (N == 1) {
+			cout << "1" << '\n';
+		} else if (N == 2) {
+			if (C == 2) {
+				cout << "1 1" << '\n';
+			} else if (A == 2) {
+				cout << "1 2" << '\n';
+			} else if (B == 2) {
+				cout << "2 1" << '\n';
+			} else assert(false);
+		} else {
+			vector<int> res; res.reserve(N);
+			for (int i = 0; i < A-C; i++) res.push_back(2);
+			for (int i = 0; i < C; i++) res.push_back(3);
+			for (int i = 0; i < B-C; i++) res.push_back(2);
+			int extra = N - (A + B - C);
+			if (extra > 0) {
+				res.insert(res.begin()+1, extra, 1);
+			}
+			for (int i = 0; i < N; i++) {
+				cout << res[i] << " \n"[i+1==N];
+			}
+		}
+	}
+
+	return 0;
 }
