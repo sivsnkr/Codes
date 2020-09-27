b17f501fb2be7642159940005d74ea26e21aa0ed
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-09-27 06:49:51 +0530

    update

--- Codeforces/1.cpp
+++ Codeforces/1.cpp
@@ -11,7 +11,7 @@ const char NL = '\n';
 #define all(a) a.begin(), a.end()
 #define size(container) (int)container.size()
 #define int long long int
-#define pb push_back
+// #define pb push_back
 #define fh freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
 int mod = 1000000007;
 clock_t startTime;
@@ -23,9 +23,58 @@ void read(vector<int> &a)
     for(auto &it : a)cin>>it;
 }
 
+#define pb insert
 inline void solve()
 {
     // all the code goes here
+    test
+    {
+        int n;cin>>n;
+        set<int> xl,xu,yl,yu;
+        bool ok = 1;
+        f(i,0,n)
+        {
+            int x,y;cin>>x>>y;
+            int f1,f2,f3,f4;
+            cin>>f1>>f2>>f3>>f4;
+            if(f1 == 0)
+            {
+                if(size(xl) > 0 && x < *xl.rbegin())
+                    ok = 0;
+                xl.pb(x);
+            }
+            if(f2 == 0)
+            {
+                if(size(yu) > 0 && y > *yu.rbegin())
+                    ok = 0;
+                yu.pb(y);
+            }
+            if(f3 == 0)
+            {
+                if(size(xu) > 0 && x > *xu.rbegin())
+                    ok = 0;
+                xu.pb(x);
+            }
+            if(f4 == 0)
+            {
+                if(size(yl) > 0 && y < *yl.rbegin())
+                    ok = 0;
+                yl.pb(y);
+            }
+        }
+        cout<<"ok "<<ok<<NL;
+        cout<< *yu.begin()<<" "<<*yl.rbegin()<<NL;
+        cout<< *xu.begin()<<" "<<*xl.rbegin()<<NL;
+        if(!ok || (size(yu) > 0 && size(yl) > 0 && *yu.begin() < *yl.rbegin()) || (size(xu) > 0 && size(xl) > 0 && *xu.begin() < *xl.rbegin()))
+            cout<<0;
+        else
+        {
+            int x = size(xu) != 0 ? *xu.begin():size(xl) != 0 ?*xl.begin():0;
+            int y = size(yu) != 0 ? *yu.begin():size(yl) != 0 ?*yl.begin():0;
+            cout<<1<<" "<<x<<" "<<y;
+        }
+        cout<<NL;
+    }
 }
 
 int32_t main()
