9f574d3e4c1d83de203833d327c89249cdfe1cc4
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-10-03 16:20:50 +0530

    update

--- Codeforces/1.cpp
+++ Codeforces/1.cpp
@@ -11,21 +11,89 @@ const char NL = '\n';
 #define all(a) a.begin(), a.end()
 #define size(container) (int)container.size()
 #define pb push_back
-#define int LL
+// #define int LL
 #define fh freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
 int mod = 1000000007;
 clock_t startTime;
 double getCurrentTime() {
 	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
 }
+
+#define int long double
 void read(vector<int> &a)
 {
     for(auto &it : a)cin>>it;
 }
 
+const int MX = 1e5+5;
+int n,m,d;
+vector<vector<pair<int,int>>> g(MX);
+int num;
+vector<int> re;
+bool dfs(int root, bool print,int plength,int cnum)
+{
+    // if(plength > 0)
+    //     cout<<"val "<<(cnum/plength)<<NL;
+    if(plength > 0 && cnum/plength > num)
+        return false;
+    
+    // cout<<"root size "<<root<<" "<<size(g[root])<<NL;
+    if(root == n)
+    {
+        if(print)
+            re.pb(root);
+        return true;
+    }
+    bool res = false;
+    for(auto[child,num] : g[root])
+    {
+        // cout<<"child "<<child<<NL;
+        res = (res || dfs(child,print,plength+1,cnum+num));
+    }
+    // cout<<"res root "<<res<<" "<<root<<NL;
+    if(res && print)
+        re.pb(root);
+    return res;
+}
+
+bool good(int m)
+{
+    // cout<<"m "<<m<<NL;
+    num = m;
+    return dfs(1,0,0,0);
+}
+
 inline void solve()
 {
     // all the code goes here
+    cin>>n>>m;
+    f(i,0,m)
+    {
+        int x,y,c;cin>>x>>y>>c;
+        g[x].pb({y,c});
+    }
+
+    int ans = -1,l = 0,r = 1e10;
+    f(i,0,100)
+    {
+        int m = (l+r)/2;
+        if(good(m))
+        {
+            ans = m;
+            r = m-1;
+        }
+        else
+        {
+            l = m+1;
+        }
+    }
+    // cout<<"ans "<<ans<<NL;
+    num = ans;
+    dfs(1,1,0,0);
+    cout<<size(re)-1<<NL;
+    reverse(all(re));
+    f(i,0,size(re))
+        cout<<re[i]<<" \n"[i==size(re)-1];
 }
 
 int32_t main()
--- Codeforces/2.cpp
+++ Codeforces/2.cpp
@@ -11,7 +11,8 @@ const char NL = '\n';
 #define all(a) a.begin(), a.end()
 #define size(container) (int)container.size()
 #define pb push_back
-#define int LL
+// #define int LL
+#define int long double
 #define fh freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
 int mod = 1000000007;
 clock_t startTime;
@@ -23,98 +24,52 @@ void read(vector<int> &a)
     for(auto &it : a)cin>>it;
 }
 
-int n;
-vector<int> a;
+int n,k;
+vector<pair<int,int>> a;
 
 bool good(int m)
 {
-    vector<pair<int,int>> res;
+    // cout<<"m "<<m<<NL;
+    vector<int> re(n);
     f(i,0,n)
     {
-        if(a[i] == -1)
-        {
-            if(i-1 >= 0 && a[i-1] != -1)
-            {
-                res.pb({a[i-1]-m,a[i-1]+m});
-            }
-            if(i+1 < n && a[i+1] != -1)
-            {
-                res.pb({a[i+1]-m,a[i+1]+m});
-            }
-        }
+        auto [x,y] = a[i];
+        re[i] = x-m*y;
     }
-    int l = 0,r = 1e9;
-    for(auto [f,s] : res)
-    {
-        l = max(l,f);
-        r = min(r,s);
-    }
-    bool ret = r >= l;
-    f(i,0,n-1)
-    {
-        if(a[i] != -1 && a[i+1] != -1)
-        {
-            if(abs(a[i]-a[i+1]) > m)
-                return false;
-        }
-    }
-    return ret;
+    sort(all(re),greater<int>());
+    int sum = 0;
+    f(i,0,k)
+        sum+=re[i];
+    // cout<<"sum "<<sum<<NL;
+    return sum >= 0;
 }
 
 inline void solve()
 {
     // all the code goes here
-    test
+    cin>>n>>k;
+    f(i,0,n)
     {
-        cin>>n;
-        a.resize(n);
-        read(a);
-        int l = 0,r = 1e9,ans = 0;
-        while(l <= r)
+        int x,y;cin>>x>>y;
+        a.emplace_back(x,y);
+    }
+
+    int ans = 0,l = 0,r = 1e11;
+    f(i,0,100)
+    {
+        // cout<<l<<" "<<r<<NL;
+        int m = (l+r)/2;
+        if(good(m))
         {
-            int m = (l+r)/2;
-            if(good(m))
-            {
-                ans = m;
-                r = m-1;
-            }
-            else
-            {
-                l = m+1;
-            }
-        }
-        vector<pair<int,int>> res;
-        int m = ans;
-        f(i,0,n)
-        {
-            if(a[i] == -1)
-            {
-                if(i-1 >= 0 && a[i-1] != -1)
-                {
-                    res.pb({a[i-1]-m,a[i-1]+m});
-                }
-                if(i+1 < n && a[i+1] != -1)
-                {
-                    res.pb({a[i+1]-m,a[i+1]+m});
-                }
-            }
-        }
-        l = 0,r = 1e9;
-        for(auto [f,s] : res)
-        {
-            l = max(l,f);
-            r = min(r,s);
-        }
-        if(r >= l)
-        {
-            cout<<ans<<" "<<l;
+            ans = m;
+            l = m+1;
         }
         else
         {
-            cout<<-1;
+            r = m-1;
         }
-        cout<<NL;
     }
+    cout<<setprecision(20)<<ans<<NL;
 }
 
 int32_t main()
