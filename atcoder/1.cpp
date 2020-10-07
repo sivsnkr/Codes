207bd0fb3c6e660032ed41d86ae472347ff3835f
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-10-07 23:47:02 +0530

    update

--- atcoder/1.cpp
+++ atcoder/1.cpp
@@ -1,12 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long LL;
-typedef long L;
 const char NL = '\n';
 #define PI 3.14159265
 #define f(i, a, b) for (int i = a; i < b; i++)
 #define fr(i, a, b) for (int i = a; i >= b; i--)
-#define testf int t;scanf("%d", &t);while (t--)
 #define test int t;cin >> t;while (t--)
 #define all(a) a.begin(), a.end()
 #define size(container) (int)container.size()
@@ -26,22 +23,6 @@ void read(vector<int> &a)
 inline void solve()
 {
     // all the code goes here
-    int n,W;cin>>n>>W;
-    vector<int> dp(W+1,0);
-
-    f(i,0,n)
-    {
-        int weight,value;
-        cin>>weight>>value;
-        fr(weight_already,W-weight,0)
-        {
-            dp[weight_already+weight] = max(dp[weight_already+weight],dp[weight_already]+value);
-        }
-    }
-    int mx = 0;
-    f(i,0,W+1)
-        mx = max(mx,dp[i]);
-    cout<<mx<<NL;
 }
 
 int32_t main()
--- cp_template.cpp
+++ cp_template.cpp
@@ -1,18 +1,9 @@
 #include <bits/stdc++.h>
-using namespace std;
-typedef long long LL;
-typedef long L;
 const char NL = '\n';
 #define PI 3.14159265
-#define f(i, a, b) for (int i = a; i < b; i++)
-#define fr(i, a, b) for (int i = a; i >= b; i--)
-#define testf int t;scanf("%d", &t);while (t--)
 #define test int t;cin >> t;while (t--)
 #define all(a) a.begin(), a.end()
-#define size(container) (int)container.size()
-#define pb push_back
 #define int LL
-#define fh freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
 int mod = 1000000007;
 clock_t startTime;
 double getCurrentTime() {
@@ -34,7 +25,8 @@ int32_t main()
     cin.tie(0);
     cout.tie(0);
     #ifndef ONLINE_JUDGE
-        fh;
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
     #endif
     startTime = clock();
     solve();
