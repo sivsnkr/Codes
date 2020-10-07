9181b94ec800ce7f18fa3ac771773fad9565ae36
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-10-07 14:30:48 +0530

    update

---
+++ atcoder/1.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef long L;
+const char NL = '\n';
+#define PI 3.14159265
+#define f(i, a, b) for (int i = a; i < b; i++)
+#define fr(i, a, b) for (int i = a; i >= b; i--)
+#define testf int t;scanf("%d", &t);while (t--)
+#define test int t;cin >> t;while (t--)
+#define all(a) a.begin(), a.end()
+#define size(container) (int)container.size()
+#define pb push_back
+#define int LL
+#define fh freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+int mod = 1000000007;
+clock_t startTime;
+double getCurrentTime() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+void read(vector<int> &a)
+{
+    for(auto &it : a)cin>>it;
+}
+
+inline void solve()
+{
+    // all the code goes here
+    int n,W;cin>>n>>W;
+    vector<int> dp(W+1,0);
+
+    f(i,0,n)
+    {
+        int weight,value;
+        cin>>weight>>value;
+        fr(weight_already,W-weight,0)
+        {
+            dp[weight_already+weight] = max(dp[weight_already+weight],dp[weight_already]+value);
+        }
+    }
+    int mx = 0;
+    f(i,0,W+1)
+        mx = max(mx,dp[i]);
+    cout<<mx<<NL;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        fh;
+    #endif
+    startTime = clock();
+    solve();
+    fflush(stdin);
+    fflush(stdout);
+    return 0;
+}
