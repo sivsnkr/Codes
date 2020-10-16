6c7f8e45e5a7e70bb239a8519757dcf22003f5c4
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-10-16 11:25:58 +0530

    update

--- Codeforces/1.cpp
+++ Codeforces/1.cpp
@@ -8,43 +8,62 @@ const char NL = '\n';
 int mod = 1000000007;
 void read(vector<int> &a);
 
+const int MX = 3e5+5;
+vector<int> p(MX);
+vector<int> ranks(MX);
+vector<int> depth(MX);
+int sum;
+int get(int c)
+{
+    sum+=depth[c];
+    return p[c] = p[c] == c ? c : get(p[c]);
+}
+
+void join(int a, int b)
+{
+    int pa = get(a);
+    depth[a] = sum+1;
+    sum = 0;
+    int pb = get(b);
+    depth[b] = sum+1;
+    if(pa != pb)
+    {
+        // sum = 0;
+        if(ranks[pa] >= ranks[pb])
+            ranks[pb] = ranks[pa]+1;
+        p[pa] = pb;
+        depth[pa] = depth[pb]+1;
+    }
+}
+
 inline void solve()
 {
     // all the code goes here
+    iota(all(p),0);
     int n,m;cin>>n>>m;
-    vector<int> a(n),b(n);
-    read(a),read(b);
-
-    sort(all(b));
-    vector<int> choices;
-    for(int i = 0; i < n; i++)
+    while(m--)
     {
-        int val = b[0]-a[i];
-        while(val < 0)
-            val+=m;
-        choices.push_back(val);
-    }
-    int ans;
-    sort(all(choices));
-    for(int choice : choices)
-    {
-        bool valid = 1;
-        vector<int> tem = a;
-        for(int i = 0; i < n; i++)
-            tem[i] = (tem[i]+choice)%m;
-        sort(all(tem));
-        for(int i = 0; i < n; i++)
+        int q;cin>>q;
+        if(q == 1)
         {
-            if(tem[i] != b[i])
-                valid = 0;
+            int a,b;cin>>a>>b;
+            join(a,b);
         }
-        if(valid)
+        else
         {
-            ans = choice;
-            break;
+            int a;cin>>a;
+            sum = 0;
+            // int pa = a;
+            get(a);
+            // while(true)
+            // {
+            //     int par = get(pa);
+            //     if(pa == par)
+            //         break;
+            // }
+            cout<<sum<<NL;
         }
     }
-    cout<<ans<<NL;
 }
 
 int32_t main()
