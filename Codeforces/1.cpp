90e43a7bb3a94978979e8bf3b064f85bbdda63d0
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-10-22 09:32:33 +0530

    update

--- Codeforces/1.cpp
+++ Codeforces/1.cpp
@@ -3,54 +3,24 @@ using namespace std;
 const char NL = '\n';
 #define test int t;cin >> t;while (t--)
 #define all(a) a.begin(), a.end()
+#define int long long
 void read(vector<int> &a);
 
 inline void solve()
 {
     // all the code goes here
-    string a,b;cin>>a>>b;
-    int ones = 0,zeros = 0;
-    int n = a.length(),m = b.length();
-    vector<int> z(n),o(n);
-    for(int i = 0; i < n; i++)
-    {
-        if(a[i] == '1')
-            ones++;
-        else
-            zeros++;
-        z[i] = zeros;
-        o[i] = ones;
-    }
+    int n,p,q,r;cin>>n>>p>>q>>r;
+    vector<int> a(n);
+    read(a);
+    vector<vector<int>> dp(n+1,vector<int>(3,LLONG_MIN));
 
-    int count = 0;
-    for(int i = 0; i < n-1; i++)
+    for(int i = 1; i <= n; i++)
     {
-        if(b[i] == '1')
-        {
-            count+=z[i];
-        }
-        else
-        {
-            count+=o[i];
-        }
+        dp[i][0] = max(dp[i-1][0],p*a[i-1]);
+        dp[i][1] = max(dp[i-1][1],dp[i][0]+q*a[i-1]);
+        dp[i][2] = max(dp[i-1][2],dp[i][1]+r*a[i-1]);
     }
-    cout<<"first count "<<count<<NL;
-    for(int i = m-1; i+n > m; i--)
-    {
-        if(b[i] == '0')
-            count+=(ones-o[m-1-i]);
-        else
-            count+=(zeros-z[m-1-i]);
-    }
-    cout<<"second count "<<count<<NL;
-    for(int i = n-1; i+n <= m; i++)
-    {
-        if(b[i] == '1')
-            count+=zeros;
-        else
-            count+=ones;
-    }
-    cout<<count<<NL;
+    cout<<dp[n][2]<<NL;
 }
 
 int32_t main()
