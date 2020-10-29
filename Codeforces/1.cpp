1cf8df3cfa2ffad16423475efcef42fd0df42e09
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-10-29 18:19:54 +0530

    update

--- Codeforces/1.cpp
+++ Codeforces/1.cpp
@@ -5,56 +5,9 @@ const char NL = '\n';
 #define all(a) a.begin(), a.end()
 void read(vector<int> &a);
 
-
-int n,k;
-const int MX = 2e5+5;
-vector<vector<int>> g(MX);
-vector<bool> vis(MX);
-int sum;
-void dfs(int root,int len,int par)
-{
-    if(vis[root])
-        sum+=len;
-    for(int i : g[root])
-        if(i != par)
-            dfs(i,len+(vis[i]?0:1),root);
-}
-
 inline void solve()
 {
     // all the code goes here
-    cin>>n>>k;
-    for(int i = 0; i < n-1; i++)
-    {
-        int x,y;cin>>x>>y;
-        g[x].push_back(y),g[y].push_back(x);
-    }
-
-    queue<pair<int,int>> st;
-    st.push({1,1});
-    vector<int> res;
-    while(!st.empty())
-    {
-        auto [top,par] = st.front();
-        st.pop();
-        for(auto c : g[top])
-            if(c != par)
-                st.push({c,top});
-        // if()
-        res.push_back(top);
-    }
-
-    int i = res.size()-1;
-    while(k--)
-    {
-        vis[res[i]] = 1;
-        i--;
-    }
-    for(int i = 1; i <= n; i++)
-        if(vis[i])
-            cout<<"r "<<i<<NL;
-    dfs(1,1,1);
-    cout<<sum<<NL;
 }
 
 int32_t main()
--- Codeforces/2.cpp
+++ Codeforces/2.cpp
@@ -1,94 +1,31 @@
-#include <iostream>
-#include <cstdio>
-#include <cstdlib>
-#include <algorithm>
-#include <cmath>
-#include <vector>
-#include <set>
-#include <map>
-#include <unordered_set>
-#include <unordered_map>
-#include <queue>
-#include <ctime>
-#include <cassert>
-#include <complex>
-#include <string>
-#include <cstring>
-#include <chrono>
-#include <random>
-#include <bitset>
+#include <bits/stdc++.h>
 using namespace std;
+const char NL = '\n';
+#define test int t;cin >> t;while (t--)
+#define all(a) a.begin(), a.end()
+void read(vector<int> &a);
 
-#ifdef LOCAL
-	#define eprintf(...) fprintf(stderr, __VA_ARGS__);fflush(stderr);
-#else
-	#define eprintf(...) 42
-#endif
-
-using ll = long long;
-using ld = long double;
-using uint = unsigned int;
-using ull = unsigned long long;
-template<typename T>
-using pair2 = pair<T, T>;
-using pii = pair<int, int>;
-using pli = pair<ll, int>;
-using pll = pair<ll, ll>;
-mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
-ll myRand(ll B) {
-	return (ull)rng() % B;
-}
-
-#define pb push_back
-#define mp make_pair
-#define all(x) (x).begin(),(x).end()
-#define fi first
-#define se second
-
-clock_t startTime;
-double getCurrentTime() {
-	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
-}
-
-const int N = 200200;
-int n;
-int a[N];
-int b[N];
-
-void solve() {
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
-		scanf("%d", &a[i]);
-	b[0] = 1;
-	int m = 1;
-	int l = 1;
-	while(l < n) {
-		int k = b[m - 1];
-		b[m] = 0;
-		while(k > 0) {
-			k--;
-			if (l == n) continue;
-			l++;
-			b[m]++;
-			while(l < n && a[l] > a[l - 1]) {
-				l++;
-				b[m]++;
-			}
-		}
-		m++;
-	}
-	printf("%d\n", m - 1);
-}
-
-int main()
+inline void solve()
 {
-	startTime = clock();
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
+    // all the code goes here
+}
 
-	int t;
-	scanf("%d", &t);
-	while(t--) solve();
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    solve();
+    fflush(stdin);
+    fflush(stdout);
+    return 0;
+}
 
-	return 0;
+void read(vector<int> &a)
+{
+    for(auto &it : a)cin>>it;
 }
