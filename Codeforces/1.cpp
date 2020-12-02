7fa01f9ca9843817b7ce9809743e39d19adf4c27
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-12-02 14:21:43 +0530

    update

--- Codeforces/1.cpp
+++ Codeforces/1.cpp
@@ -11,6 +11,69 @@ void read(vector<int> &a);
 inline void solve()
 {
     // all the code goes here
+    test
+    {
+        int n;cin>>n;
+        vector<int> a(n);
+        read(a);
+        sort(all(a));
+        int mi = 0;
+        map<int,int> st,tim,fre;
+        f(i,0,n)
+        {
+            st[a[i]] = i;
+            fre[a[i]]++;
+        }
+        f(i,0,n)
+        {
+            int mis = LLONG_MAX;
+            int mit = 0;
+            f(ti,max(mi,(int)1),201)
+            {
+                int sum = 0;
+                // int tim = ti;
+                f(j,i,n)
+                {
+                    sum+=abs(ti+(j-i)-a[j]);
+                    // tim;
+                }
+                // cout<<"sum "<<sum<<NL;
+                // cout<<"ti "<<ti<<NL;
+                if(mis > sum)
+                {
+                    mis = sum;
+                    mit = ti;
+                }
+            }
+            // cout<<"mis "<<mis<<NL;
+            // cout<<"mit "<<mit<<NL;
+            mi = mit+fre[a[i]];
+            tim[a[i]] = mit;
+            i = st[a[i]];
+        }
+        // for(auto [x,y] : tim)
+        // {
+        //     cout<<x<<" "<<y<<NL;
+        // }
+        int res = 0;
+        int time = tim[a[0]];
+        f(i,0,n)
+        {
+            while(i < n-1 && a[i] == a[i+1])
+            {
+                cout<<a[i]<<" "<<time<<NL;
+                res+=abs(time-a[i]);
+                i++;
+                time++;
+            }
+            cout<<a[i]<<" "<<time<<NL;
+            res+=abs(time-a[i]);
+            time = tim[a[i+1]];
+        }
+        // cout<<"res "<<res<<NL;
+        // cout<<time<<" "<<a[n-1]<<NL;
+        cout<<res<<NL;
+    }
 }
 
 int32_t main()
--- Codeforces/2.cpp
+++ Codeforces/2.cpp
@@ -1,4 +1,3 @@
-// problem url https://codeforces.com/gym/102694/problem/C
 #include <bits/stdc++.h>
 using namespace std;
 const char NL = '\n';
@@ -9,111 +8,40 @@ const char NL = '\n';
 #define int long long
 void read(vector<int> &a);
 
-int L;
-vector<vector<int>> g;
-vector<vector<int>> up;
-vector<int> tin,tout;
-vector<int> depths;
-int timer;
-
-void dfs(int root, int par,int depth)
-{
-    tin[root] = ++timer;
-    up[root][0] = par;
-    depths[root] = depth;
-    f(i,1,L+1)
-    {
-        up[root][i] = up[up[root][i-1]][i-1];
-    }
-
-    for(int i : g[root])
-    {
-        if(i != par)
-        {
-            dfs(i,root,depth+1);
-        }
-    }
-
-    tout[root] = ++timer;
-}
-
-bool is_ansistor(int u, int v)
-{
-    if(tin[u] <= tin[v] && tout[u] >= tout[v])
-        return 1;
-    return 0;
-}
-
-int dist_lca(int root,int lca)
-{
-    if(is_ansistor(root,lca))
-        return root;
-    else if(is_ansistor(lca,root))
-        return lca;
-    fr(i,L,0)
-    {
-        if(!is_ansistor(up[root][i],lca))
-        {
-            root = up[root][i];
-        }
-    }
-    return up[root][0];
-}
-
-int calc_dist(int root,int dist)
-{
-    int val = root;
-    if(dist == 0)
-        return val;
-    while(dist > 0)
-    {
-        int p = 0;
-        while(pow(2,p) <= dist)
-            p++;
-        dist-=pow(2,p-1);
-        val = up[val][p-1];
-    }
-    return val;
-}
-
 inline void solve()
 {
     // all the code goes here
-
-    int n;cin>>n;
-    L = ceil(log2(n));
-    g.resize(n+1);
-    up.assign(n+1,vector<int>(L+1));
-    depths.resize(n+1);
-    tin.resize(n+1),tout.resize(n+1);
-    f(i,0,n-1)
+    const int MX = 1e6+6;
+    vector<int> dp(MX,0);
+    f(i,2,MX)
     {
-        int x,y;cin>>x>>y;
-        g[x].push_back(y),g[y].push_back(x);
-    }
-    dfs(1,1,0);
-    int q;cin>>q;
-    while(q--)
-    {
-        int a,b,c;cin>>a>>b>>c;
-        int lca = dist_lca(a,b);
-        int dist = depths[a]-depths[lca];
-        int total_dist = depths[a]+depths[b]-2*depths[lca];
-        if(c < total_dist)
+        if(i*i < MX && dp[i] == 0)
         {
-            if(c <= dist)
-            {
-                // on his side of lca
-                cout<<calc_dist(a,c)<<NL;
-            }
+            if(dp[i*i] == 1)
+                dp[i*i]++;
             else
             {
-                // on the other side of lca
-                cout<<calc_dist(b,total_dist-c)<<NL;
+                dp[i*i]+=2;
             }
         }
-        else
-            cout<<b<<NL;
+        for(int j = i*i; j < MX; j+=i)
+        {
+            if(dp[j] == 0)
+            {
+                dp[j]++;
+            }
+        }
+    }
+
+    f(i,1,MX)
+    {
+        dp[i]+=dp[i-1];
+    }
+    int q;cin>>q;
+    f(i,0,q)
+    {
+        int x;cin>>x;
+        cout<<x-dp[x]<<NL;
     }
 }
 
