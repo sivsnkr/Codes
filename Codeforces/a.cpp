528bc8a9c2da349129249b2dd27258aa54c5403a
Shiv Shankar <ced17i026@iiitdm.ac.in>
2021-01-16 20:23:51 +0530

    update

--- Codeforces/a.cpp
+++ Codeforces/a.cpp
@@ -9,34 +9,10 @@ using namespace std;
 #define int long long
 template<typename T>
 void read(vector<T> &a);
-int inf = 1e15;
+
 inline void solve()
 {
     // all the code goes here
-    int n;cin>>n;
-    int dp[n+1][8];
-    f(i,0,n+1)
-        f(j,0,8)
-            dp[i][j] = inf;
-    dp[0][0] = 0;
-    f(i,0,n)
-    {
-        int n;string s;
-        cin>>n>>s;
-        int s_mask = 0;
-        f(pos,0,3)
-        {
-            if(find(all(s),'A'+pos) != s.end())
-                s_mask+=((int)1<<pos);
-        }
-        f(mask,0,8)
-        {
-            dp[i+1][mask] = min(dp[i+1][mask],dp[i][mask]);
-            dp[i+1][mask|s_mask] = min(dp[i+1][mask|s_mask],dp[i][mask]+n);
-        }
-    }
-
-    cout<<(dp[n][7] >= inf ? -1 : dp[n][7])<<NL;
 }
 
 int32_t main()
--- Codeforces/e.cpp
+++ Codeforces/e.cpp
@@ -10,9 +10,56 @@ using namespace std;
 template<typename T>
 void read(vector<T> &a);
 
+struct ele
+{
+    int a,b,c;
+};
 inline void solve()
 {
     // all the code goes here
+    int n;cin>>n;
+    vector<ele> a(n);
+    f(i,0,n)
+    {
+        int x,y;cin>>x>>y;
+        a[i] = {x,y,i};
+    }
+    sort(all(a),[](ele a, ele b)->bool{
+        return a.a < b.a;
+    });
+    int k;cin>>k;
+    vector<int> kval(k);
+    read(kval);
+    sort(all(kval));
+    
+    vector<vector<int>> dp(n+1,vector<int>(k+1,0));
+    vector<int> pos(k,-1);
+    f(i,0,n+1)
+    {
+        f(j,0,k+1)
+        {
+            if(i < n && j < k && a[i].a <= kval[j])
+            {
+                if(dp[i+1][j+1] < dp[i][j]+a[i].b)
+                    pos[j] = a[i].c+1;
+                dp[i+1][j+1] = max(dp[i+1][j+1],dp[i][j]+a[i].b);
+            }
+            if(j < k)
+                dp[i][j+1] = max(dp[i][j+1],dp[i][j]);
+            if(i < n)
+                dp[i+1][j] = max(dp[i+1][j],dp[i][j]);
+        }
+    }
+
+    f(i,0,k)
+        cout<<pos[i]<<" \n"[i==k-1];
+    map<int,int> st;
+    f(i,0,k)
+        if(pos[i] >= 0)
+            st[pos[i]] = i;
+    cout<<st.size()<<" "<<dp[n][k]<<NL;
+    for(auto [x,y] : st)
+        cout<<x<<" "<<y+1<<NL;
 }
 
 int32_t main()
