5379ef23f922da7fa8adaeef92b788a5039f05c5
Shiv Shankar <ced17i026@iiitdm.ac.in>
2021-01-29 15:23:55 +0530

    update

--- Codeforces/a.cpp
+++ Codeforces/a.cpp
@@ -12,55 +12,6 @@ void read(vector<T> &a);
 inline void solve()
 {
     // all the code goes here
-    test
-    {
-        int n,m;cin>>n>>m;
-        vector<vector<int>> a(n,vector<int>(m));
-        for(int i = 0; i < n; i++)
-            for(int j = 0; j < m; j++)
-                cin>>a[i][j];
-
-
-        vector<vector<bool>> res(n,vector<bool>(m,0));
-
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < m; j++)
-            {
-                bool dirty = 0;
-                int mx = 0;
-                if(i-1 >= 0)
-                {
-                    if(a[i-1][j] > a[i][j])
-                    {
-                        mx = max(mx,a[i-1][j]);
-                        dirty = 1;
-                    }
-                    if(j-1 >= 0 && a[i-1][j-1] > a[i][j])
-                    {
-                        mx = max(mx,a[i-1][j-1]);
-                        dirty = 1;
-                    }
-                    if(j+1 < n && a[i-1][j+1] > a[i][j])
-                    {
-                        mx = max(mx,a[i-1][j+1]);
-                        dirty = 1;
-                    }
-                }
-                if(!dirty)
-                    res[i][j] = 1;
-                a[i][j] = max(a[i][j],mx);
-            }
-        }
-
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < m; j++)
-                cout<<res[i][j];
-            cout<<NL;
-        }
-    }
-
 }
 
 int32_t main()
