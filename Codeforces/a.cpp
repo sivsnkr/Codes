3ea2deacdda322143bb2f5626c0f6b2f3acf5b9a
sivsnkr <ced17i026@iiitd.ac.in>
2021-01-29 15:57:38 +0530

    update

--- Codeforces/a.cpp
+++ Codeforces/a.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 #define NL '\n'
 #define LL long long
-#define test int t;cin >> t;while (t--)
+#define test  \
+    int t;    \
+    cin >> t; \
+    while (t--)
 #define all(a) a.begin(), a.end()
 #define int long long
-template<typename T>
+template <typename T>
 void read(vector<T> &a);
 
 inline void solve()
@@ -19,17 +22,18 @@ int32_t main()
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt","r",stdin);
-        freopen("output.txt","w",stdout);
-    #endif
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
     solve();
     fflush(stdin);
     fflush(stdout);
     return 0;
 }
-template<typename T>
+template <typename T>
 void read(vector<T> &a)
 {
-    for(auto &it : a)cin>>it;
+    for (auto &it : a)
+        cin >> it;
 }
