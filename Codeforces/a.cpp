69c817fae0e44dd17eb277c7e2f6cdf575521e60
sivsnkr <ced17i026@iiitd.ac.in>
2021-02-14 10:55:06 +0530

    update

--- Codeforces/a.cpp
+++ Codeforces/a.cpp
@@ -5,94 +5,35 @@ using namespace std;
 #define LL long long
 #define test int t;cin >> t;while (t--)
 #define all(a) a.begin(), a.end()
+#define int long long
 template<typename T>
 void read(vector<T> &a);
-
-class DSU
-{
-    vector<int> p,ranks;
-    public:
-    DSU(int MX)
-    {
-        p.resize(MX);
-        ranks.resize(MX);
-        iota(p.begin(),p.end(),0);
-    }
-    int get(int c)
-    {
-        return p[c] = p[c] == c ? c : get(p[c]);
-    }
-    void join(int a, int b)
-    {
-        int pa = get(a),pb = get(b);
-        if(pa != pb)
-        {
-            if(ranks[pa] == ranks[pb])
-                ranks[pa]++;
-            if(ranks[pa] > ranks[pb])
-                p[pb] = pa;
-            else
-                p[pa] = pb;
-        }
-    }
-};
 
 inline void solve()
 {
     // all the code goes here
-    int n,m;cin>>n>>m;
-    class DSU st(n+1);
-    vector<pair<int,int>> edges;
-    vector<int> cnt(n+1,0);
-
-
-    for(int i = 0; i < m; i++)
+    int n;cin>>n;
+    vector<int> a(n);
+    read(a);
+    cout<<"1 1"<<NL;
+    cout<<-a[0]<<NL;
+    if(n > 1)
     {
-        int x,y;cin>>x>>y;
-        edges.emplace_back(x,y);
-        cnt[x]++,cnt[y]++;
+        cout<<"1 "<<n<<NL;
+        cout<<0<<" ";
+        for(int i = 1; i < n; i++)
+            cout<<-n*a[i]<<" \n"[i==n-1];
+        cout<<"2 "<<n<<NL;
+        for(int i = 1; i < n; i++)
+            cout<<(n-1)*a[i]<<" \n"[i==n-1];
     }
-    vector<vector<int>> g(n+1);
-    multiset<pair<int,int>> min_vals;
-    for(int i = 1; i <= n; i++)
-        min_vals.insert({cnt[i],i});
-    int res = 0;
-    for(auto [x,y] : edges)
+    else
     {
-        if(st.get(x) != st.get(y))
-        {
-            res++;
-            st.join(x,y);
-            g[x].push_back(y),g[y].push_back(x);
-        }
+        cout<<"1 1"<<NL;
+        cout<<0<<NL;
+        cout<<"1 1"<<NL;
+        cout<<0<<NL;
     }
-    cout<<"val "<<res<<NL;
-    for(int i = 1; i <= n; i++)
-    {
-        if(cnt[i] < n-1)
-        {
-            auto it = min_vals.find({cnt[i],i});
-            auto [count,in] = *it;
-            min_vals.erase(it);
-            if(min_vals.size() > 0 && (*min_vals.begin()).first < n-1)
-            {
-                res--;
-                cout<<"i "<<i<<NL;
-                auto [cou,j] = *min_vals.begin();
-                cnt[j]++;
-                cout<<"j "<<j<<NL;
-                min_vals.erase(min_vals.begin());
-                min_vals.insert({cou+1,j});
-                min_vals.insert({count+1,i});
-                cnt[i]++;
-            }
-            else 
-                min_vals.insert({count,i});
-        }
-    }
-
-    cout<<res<<NL;
-
 }
 
 int32_t main()
