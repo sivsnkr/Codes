dbc65ad6ff570d01af92a09844d91e3904e5c0fb
sivsnkr <ced17i026@iiitdm.ac.in>
2021-02-20 10:09:50 +0530

    update

--- Codeforces/a.cpp
+++ Codeforces/a.cpp
@@ -10,6 +10,28 @@ using namespace std;
 inline void solve()
 {
     // let's code
+    int n;cin>>n;
+    int k;cin>>k;
+    vector<int> a(n);
+    read(a);
+
+
+    int l = 0;
+    LL res = 0;
+    map<int,int> st;
+    for(int i = 0; i < n; i++)
+    {
+        st[a[i]]++;
+        while(size(st) > k)
+        {
+            st[a[l]]--;
+            if(st[a[l]] == 0)
+                st.erase(a[l]);
+            l++;
+        }
+        res += (i-l+1);
+    }
+    cout<<res<<NL;
 }
 
 int32_t main()
@@ -17,10 +39,6 @@ int32_t main()
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt","r",stdin);
-        freopen("output.txt","w",stdout);
-    #endif
     cout<<setprecision(10);
     solve();
     fflush(stdin);
--- cp_template.cpp
+++ cp_template.cpp
@@ -17,10 +17,6 @@ int32_t main()
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt","r",stdin);
-        freopen("output.txt","w",stdout);
-    #endif
     cout<<setprecision(10);
     solve();
     fflush(stdin);
