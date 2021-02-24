17847da5a5717ab0a9dd9193b875fd7f53d1c946
sivsnkr <ced17i026@iiitdm.ac.in>
2021-02-24 09:07:26 +0530

    update

--- Codeforces/b.cpp
+++ Codeforces/b.cpp
@@ -6,63 +6,59 @@ using namespace std;
 #define test int t;cin >> t;while (t--)
 #define all(a) a.begin(), a.end()
 #define read(a) for(int poi = 0; poi < size(a); poi++)cin>>a[poi]
-#define int long long
-const int mod = 998244353;
 
-void modv(int &a, int p)
+class DSU
 {
-    a%=p;
-    while(a >= p)a-=p;
-    while(a < 0)a+=p;
-}
-
-class NCR
-{
-    // O(n) for big n and r modulo prime p
-    vector<int> fact,inv,finv;
+    vector<int> p,ranks;
     public:
-    NCR(int n,int c, int p)
+    DSU(int MX)
     {
-        fact.resize(n+1),inv.resize(n+1),finv.resize(n+1);
-        fact[1] = inv[1] = finv[0] = fact[0] = finv[1] = inv[0] = 1;
-        for(int i = 2; i <= n; i++)
-            fact[i] = i*fact[i-1]%p;
-        for(int i = 2; i <= n; i++)
-            inv[i] = p-p/i*inv[p%i]%p;
-        for(int i = 2; i <= n; i++)
-            finv[i] = inv[i]*finv[i-1]%p;
+        p.resize(MX);
+        ranks.resize(MX);
+        iota(p.begin(),p.end(),0);
     }
-    int ncr(int n,int r,int p)
+    int get(int c)
     {
-        if(n < r)
-            return 0;
-        return fact[n]*finv[r]%p*finv[n-r]%p;
+        return p[c] = p[c] == c ? c : get(p[c]);
+    }
+    void join(int a, int b)
+    {
+        int pa = get(a),pb = get(b);
+        if(pa != pb)
+        {
+            if(ranks[pa] == ranks[pb])
+                ranks[pa]++;
+            if(ranks[pa] > ranks[pb])
+                p[pb] = pa;
+            else
+                p[pa] = pb;
+        }
     }
 };
 
 inline void solve()
 {
-    // let's code
-    int n;cin>>n;
-    vector<int> a(2*n);
-    read(a);
-    sort(all(a));
+    // all the code goes here
+    int n,k;cin>>n>>k;
+    vector<tuple<int,int,int>> edges(k);
+    class DSU st(n+1);
+    for(int i = 0; i < k; i++)
+    {
+        int x,y,d;cin>>x>>y>>d;
+        edges[i] = {d,x,y};
+    }
+    sort(all(edges));
     int res = 0;
-    for(int i = n; i < 2*n; i++)
+    for(int i = 0; i < k; i++)
     {
-        res += a[i];
-        modv(res,mod);
+        auto [d,x,y] = edges[i];
+        int pa = st.get(x),pb = st.get(y);
+        if(pa != pb)
+        {
+            st.join(pa,pb);
+            res = max(res,d);
+        }
     }
-
-    for(int i = 0; i < n; i++)
-    {
-        res -= a[i];
-        modv(res,mod);
-    }
-
-    class NCR st(2*n,n,mod);
-    res = res * st.ncr(2*n,n,mod);
-    res %= mod;
     cout<<res<<NL;
 }
 
