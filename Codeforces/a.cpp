b339bb14d162469d057b9d95b605b8b576d17f90
sivsnkr <ced17i026@iiitdm.ac.in>
2021-03-03 15:03:32 +0530

    update

--- Codeforces/a.cpp
+++ Codeforces/a.cpp
@@ -9,33 +9,102 @@ using namespace std;
 inline void solve()
 {
     // let's code
-    test
-    {
-        int n,k;cin>>n>>k;
-        string s;cin>>s;
+    test{
+        int n,u,r,d,l;cin>>n>>u>>r>>d>>l;
 
-        if(k%n == 0)
-        {
-            cout<<n<<NL;
-            continue;
-        }
-        map<int,int> freq;
-        for(char c : s)
-            freq[c-'a']++;
-        int res = 0;
-        for(int lens = 1; lens <= n; lens++)
-        {
-            int kval = k%lens;
-            int reqf = lens/kval;
-            map<int,int> cf = freq;
-            for(int i = 0; i < kval; i++)
-            {
-                for(auto &[x,y] : freq)
-                {
+        bool valid = 1;
 
-                }
-            }
+        if(u > n-1)
+        {
+            if(l == 0 || r == 0)
+                valid = 0;
+            if(r > 0)
+               r--;
+            if(l > 0)
+                l--;
+            u-=2;
         }
+
+        if(r > n-1)
+        {
+            if(u == 0 || d == 0)
+                valid = 0;
+            if(u > 0)
+                u--;
+            if(d > 0)
+                d--;
+            r-=2;
+        }
+
+        if(d > n-1)
+        {
+            if(l == 0 || r == 0)
+                valid = 0;
+            if(r > 0)
+                r--;
+            if(l > 0)
+                l--;
+            d-=2;
+        }
+
+        if(l > n-1)
+        {
+            if(u == 0 || d == 0)
+                valid = 0;
+            if(u > 0)
+                u--;
+            if(d > 0)
+                d--;
+            l-=2;
+        }
+
+        if(u > n-2)
+        {
+            if(r > 0)
+                r--;
+            else if(l > 0)
+                l--;
+            else
+                valid = 0;
+            u--;
+        }
+        if(l > n-2)
+        {
+            if(u > 0)
+                u--;
+            else if(d > 0)
+                d--;
+            else
+                valid = 0;
+            l--;
+        }
+
+        if(r > n-2)
+        {
+            if(u > 0)
+                u--;
+            else if(d > 0)
+                d--;
+            else
+                valid = 0;
+            r--;
+        }
+        if(d > n-2)
+        {
+            if(l > 0)
+                l--;
+            else if(r > 0)
+                r--;
+            else
+                valid = 0;
+            d--;
+        }
+
+        if(!valid)
+            cout<<"NO";
+        else
+            cout<<"YES";
+        cout<<NL;
     }
 }
 
--- Codeforces/b.cpp
+++ Codeforces/b.cpp
@@ -1,25 +1,72 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define NL '\n'
-#define LL long long
-#define test int t;cin >> t;while (t--)
-#define all(a) a.begin(), a.end()
-#define read(a) for(int poi = 0; poi < size(a); poi++)cin>>a[poi]
-
-inline void solve()
-{
-    // let's code
-    cout<<"this is file b"<<NL;
-}
-
-int32_t main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    cout<<setprecision(10);
-    solve();
-    fflush(stdin);
-    fflush(stdout);
-    return 0;
-}
+1 1
+0
+1
+1 1
+-1
+1
+1 1
+1
+1
+2 2
+-2 0
+1 1
+2 2
+1 1
+2 2
+2 2
+-1 1
+1 2
+2 2
+-1 -2
+1 2
+2 2
+0 1
+1 2
+2 2
+0 0
+2 2
+2 2
+2 0
+1 2
+2 2
+2 -1
+2 1
+2 2
+-1 0
+2 1
+2 2
+-2 0
+1 2
+2 2
+2 -2
+2 2
+2 2
+-1 1
+1 1
+2 2
+-2 -1
+2 2
+2 2
+-1 -1
+2 1
+2 2
+0 0
+2 1
+2 2
+1 0
+1 2
+2 2
+-2 -2
+2 2
+2 2
+1 2
+2 1
+2 2
+0 2
+2 2
+2 2
+1 -1
+1 2
+2 2
+2 1
+1 1
