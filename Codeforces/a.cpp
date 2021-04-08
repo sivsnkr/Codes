84659722ee55b75e5b7268ea961c47eb9a122f78
sivsnkr <ced17i026@iiitdm.ac.in>
2021-04-08 09:31:38 +0530

    update

--- Codeforces/a.cpp
+++ Codeforces/a.cpp
@@ -8,8 +8,99 @@ using namespace std;
 clock_t startTime;
 double time_taken();
 
+struct ele{
+    int d,u,v;
+};
+
 inline void solve()
 {
+    int n,m,k;cin>>n>>m>>k;
+    vector<int> g(n);
+    read(g);
+    g.insert(g.begin(),0);
+    vector<ele> data(m);
+    for(int i = 0; i < m; i++){
+        auto &[d,u,v] = data[i];
+        cin>>u>>v>>d;
+    }
+
+    vector<int> mins(n+1,1);
+    for(int i = 1; i <= n; i++)
+        if(g[i] < 0)
+            mins[i] = 0;
+    // for(int i = 1; i <= n; i++)
+    //     cout<<mins[i]<<" \n"[i==n];
+
+    sort(all(data),[](ele a, ele b)->bool{
+        return abs(a.d) > abs(b.d);
+    });
+    
+    vector<bool> vis(n+1,0);
+    for(auto [d,u,v] : data){
+        int sum = 0;
+        for(int i = u; i <= v; i++)
+            sum += g[i];
+        int sum1 = 0;
+        for(int i = u; i <= v; i++)
+            if(mins[i])
+                sum1 += g[i];
+        bool changable = 1;
+        for(int i = u; i <= v; i++)
+            changable = changable && !vis[i];
+        if(!changable)
+            continue;
+        if(d + sum > sum1){
+            for(int i = u; i <= v; i++){
+                mins[i] = 1;
+                vis[i] = 1;
+            }
+        }
+
+        // int psum = sum;
+        // sum = 0;
+        // for(int i = u; i <= v; i++){
+        //     if(mins[i])
+        //         sum += g[i];
+        //     else 
+        //         break;
+        // }
+
+        if(sum == sum1){
+            int mi = g[u];
+            int in = u;
+            for(int i = u; i <= v; i++){
+                if(g[u] < mi){
+                    mi = g[u];
+                    in = i;
+                }
+            }
+            if(sum + d < sum-mi){
+                mins[in] = 0;
+                vis[in] = 1;
+            }
+        }
+
+        // for(int i = 1; i <= n; i++)
+        //     cout<<mins[i]<<" \n"[i==n];
+    }
+
+    int res = 0;
+    for(int i = 1; i <= n; i++)
+        if(mins[i])
+            res += g[i];
+
+    for(auto [d,u,v] : data){
+        bool re = 1;
+        for(int i = u; i <= v; i++){
+            re  = re && mins[i];
+        }
+        if(re)
+            res += d;
+    }
+
+    // for(int i = 1; i <= n; i++)
+    //     cout<<mins[i]<<" \n"[i==n];
+    cout<<res<<NL;
 }
 
 int32_t main()
@@ -20,6 +111,7 @@ int32_t main()
     cout.tie(nullptr);
     cout<<setprecision(10);
     int t = 1;
+    cin>>t;
     while(t--){
         solve();
     }
--- Codeforces/main.cpp
+++ Codeforces/main.cpp
@@ -4,26 +4,117 @@ using namespace std;
 #define LL long long
 #define all(a) (a).begin(), (a).end()
 #define read(a) for(int poi = 0; poi < size(a); poi++)cin>>(a)[poi]
-#define size(a) (int)(a).size()
 clock_t startTime;
 double time_taken();
-
-struct ele{
-    int u,v,d;
-};
+#define size(a) (int)(a).size()
+vector<vector<int>> res;
+int n;
 
 inline void solve()
 {
-    int n,m,k;cin>>n>>m>>k;
-    vector<int> g(n);
-    read(g);
-    vector<ele> data(m);
-    for(int i = 0; i < m; i++){
-        auto &[u,v,d] = data[i];
-        cin>>u>>v>>d;
+    cin>>n;
+    int en = n*n;
+    res.assign(n,vector<int>(n,0));
+    vector<vector<bool>> board(n,vector<bool>(n,0));
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(i == 0 && j == 0)
+                continue;
+            if(j == 0){
+                board[i][j] = !board[i-1][j];
+            }else{
+                board[i][j] = !board[i][j-1];
+            }
+        }
     }
 
-    
+    for(int i = 0; i < en; i++){
+        int te;cin>>te;
+        bool found = 0;
+        if(te == 1){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    if(res[i][j] == 0 && board[i][j]){
+                        res[i][j] = 2;
+                        cout<<res[i][j]<<" "<<i+1<<" "<<j+1<<endl;
+                        found = 1;
+                        break;
+                    }
+                }
+                if(found)
+                        break;
+            }
+            if(!found){
+                for(int i = 0; i < n; i++){
+                    for(int j = 0; j < n; j++){
+                        if(res[i][j] == 0 && !board[i][j]){
+                            res[i][j] = 3;
+                            cout<<res[i][j]<<" "<<i+1<<" "<<j+1<<endl;
+                            found = 1;
+                            break;
+                        }
+                    }
+                }
+                if(found)
+                        break;
+            }
+        }else if(te == 2){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    if(res[i][j] == 0 && !board[i][j]){
+                        res[i][j] = 1;
+                        cout<<res[i][j]<<" "<<i+1<<" "<<j+1<<endl;
+                        found = 1;
+                        break;
+                    }
+                }
+                if(found)
+                        break;
+            }
+            if(!found){
+                for(int i = 0; i < n; i++){
+                    for(int j = 0; j < n; j++){
+                        if(res[i][j] == 0 && board[i][j]){
+                            res[i][j] = 3;
+                            cout<<res[i][j]<<" "<<i+1<<" "<<j+1<<endl;
+                            found = 1;
+                            break;
+                        }
+                    }
+                    if(found)
+                        break;
+                }
+            }
+        }else{
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    if(res[i][j] == 0 && !board[i][j]){
+                        res[i][j] = 1;
+                        cout<<res[i][j]<<" "<<i+1<<" "<<j+1<<endl;
+                        found = 1;
+                        break;
+                    }
+                }
+                if(found)
+                        break;
+            }
+            if(!found){
+                for(int i = 0; i < n; i++){
+                    for(int j = 0; j < n; j++){
+                        if(res[i][j] == 0 && board[i][j]){
+                            res[i][j] = 2;
+                            cout<<res[i][j]<<" "<<i+1<<" "<<j+1<<endl;
+                            found = 1;
+                            break;
+                        }
+                    }
+                    if(found)
+                        break;
+                }
+            }
+        }
+    }
 }
 
 int32_t main()
@@ -34,7 +125,6 @@ int32_t main()
     cout.tie(nullptr);
     cout<<setprecision(10);
     int t = 1;
-    cin>>t;
     while(t--){
         solve();
     }
