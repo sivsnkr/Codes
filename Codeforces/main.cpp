9886765073d849282fcaac50bb64a91a39ce958e
sivsnkr <ced17i026@iiitd.ac.in>
2021-04-27 20:02:41 +0530

    update

--- Codeforces/main.cpp
+++ Codeforces/main.cpp
@@ -9,40 +9,31 @@ inline void solve()
 {
     LL x0,y0,ax,ay,bx,by;cin>>x0>>y0>>ax>>ay>>bx>>by;
     LL xs,ys,t;cin>>xs>>ys>>t;
+
     vector<pair<LL,LL>> dp;
-    while(x0 < xs || y0 < ys){
-        dp.emplace_back(x0,y0);
-        x0 = ax*x0+bx;
-        y0 = ay*y0+by;
-    }
-
-    LL ti = t - abs(xs-dp[sz(dp)-1].first)-abs(ys-dp[sz(dp)-1].second);
-    int val = 0;
-    if(ti >= 0)
-        val++;
-    int i;
-    LL tim = 0;
-    for(i = sz(dp)-2; i >= 0; i--){
-        LL reqt = abs(dp[i+1].first-dp[i].first)+abs(dp[i+1].second-dp[i].second);
-        tim += reqt;
-        if(ti >= reqt){
-            ti -= reqt;
-            val++;
-        }else
-            break;
-    }
-//    dp.emplace_back(x0,y0);
-
-    if(i > 0){
-        cout<<val<<NL;
-        return;
-    }
-
-    LL time = abs(xs-dp[sz(dp)-1].first)+abs(ys-dp[sz(dp)-1].second);
     while(true){
-        LL val = time+abs(x0-dp[sz(dp)-1].first)+
+        LL reqt = abs(x0-xs)+abs(y0-ys);
+        if(reqt <= t){
+            dp.emplace_back(x0,y0);
+        }
+        if(x0 >= 1e17 ||  y0 >= 1e17)
+            break;
+        x0 = x0*ax+bx;
+        y0 = y0*ay+by;
     }
-    cout<<val<<NL;
+    int ans = 0;
+    for(int l = 0; l < sz(dp); l++){
+        for(int r = l; r < sz(dp); r++){
+            LL tt = 0;
+            for(int i = l; i < r; i++){
+                tt += abs(dp[i].first-dp[i+1].first)+abs(dp[i].second-dp[i+1].second);
+            }
+            if(tt+abs(xs-dp[l].first)+abs(ys-dp[l].second) <= t || tt+abs(xs-dp[r].first)+abs(ys-dp[r].second) <= t)
+                ans = max(ans,r-l+1);
+        }
+    }
+
+    cout<<ans<<NL;
 }
 
 int32_t main()
