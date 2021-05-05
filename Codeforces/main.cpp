eeed2ca8f7e2905c81f14645673ee7729cdf7bfc
sivsnkr <ced17i026@iiitdm.ac.in>
2021-05-05 18:56:06 +0530

    update

--- Codeforces/main.cpp
+++ Codeforces/main.cpp
@@ -5,8 +5,39 @@ using namespace std;
 #define all(a) (a).begin(), (a).end()
 #define sz(a) (int)(a).size()
 
+void print(vector<int> &a){
+    for(int i : a)
+        cout<<i<<" ";
+    cout<<NL;
+}
+
 inline void solve()
 {
+    const int mod = 1e9+7;
+    string n;cin>>n;
+    int m;cin>>m;
+    vector<int> freq(10,0);
+    for(char c : n){
+        freq[c-'0']++;
+    }
+    
+    for(int i = 0; i < m; i++){
+        vector<int> new_freq = freq;
+        for(int j = 0; j < 9; j++){
+            new_freq[j+1] = freq[j];
+        }
+        new_freq[1] = (new_freq[1]+freq[9])%mod;
+        new_freq[0] = freq[9];
+        freq = new_freq;
+        // print(freq);
+    }
+
+    LL res = 0;
+    for(int i = 0; i < 10; i++){
+        res += freq[i];
+        res %= mod;
+    }
+    cout<<res<<NL;
 }
 
 int32_t main()
@@ -18,6 +49,7 @@ int32_t main()
     cout.tie(nullptr);
     cout<<setprecision(10);
     int t = 1;
+    cin>>t;
     while(t--){
         solve();
     }
