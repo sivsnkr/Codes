0edd70f55eb48e5c7e6c5df03f4fdc3ccd0c4b87
Shiv Shankar <shivshankar@Shivs-MacBook-Air.local>
2021-07-11 10:09:59 +0530

    update

--- Templates/Segment_tree.cpp
+++ Templates/Segment_tree.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 class Segment_tree
 {
+    // 0 based indexing
     private:
     vector<int> A,st;
     int n;
@@ -11,6 +12,10 @@ class Segment_tree
     void build(int p, int L, int R);
     int Access(int p,int i, int j, int L , int R);
     void Update(int p,int i,int L, int R);
+    int func(int a, int b){
+        // implement your func for combining the seg tree here
+        return gcd(a,b);
+    }
     public:
     Segment_tree(vector<int> a)
     {
@@ -31,20 +36,20 @@ void Segment_tree::Update(int p,int i, int L, int R)
     Update(right(p),i,(L+R)/2+1,R);
     int p1 = st[left(p)];
     int p2 = st[right(p)];
-    st[p] = (A[p1]>=A[p2])?p2:p1;
+    st[p] = func(p1,p2);
 }
 
 void Segment_tree::build(int p, int L, int R)
 {
     if(L == R)
     {
-        st[p] = L;
+        st[p] = A[L];
         return;
     }
     build(left(p),L,(L+R)/2);
     build(right(p),(L+R)/2+1,R);
     int p1 = st[right(p)],p2 = st[left(p)];
-    st[p] = (A[p1] >= A[p2])?p2:p1;
+    st[p] = func(p1,p2);
 }
 
 int Segment_tree::Access(int p,int i, int j, int L, int R)
@@ -57,7 +62,7 @@ int Segment_tree::Access(int p,int i, int j, int L, int R)
     int p2 = Access(right(p),i,j,(L+R)/2+1,R);
     if(p1 == -1)return p2;
     if(p2 == -1)return p1;
-    return (A[p1]>=A[p2])?p2:p1;
+    return func(p1,p2);
 }
 
 int main()
